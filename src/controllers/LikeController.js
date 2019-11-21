ab7a224c22ae21b0216963a894b52c240358cbf1
Renê Soares <reenerochase@gmail.com>
2019-11-20 21:24:45 -0300

    feat(function): Installing and configuring the match function.

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -12,6 +12,19 @@ class LikeController {
       return res.status(400)
     }
 
+    if (targetDev.likes.includes(loggedDev._id)) {
+      const loggedSocket = req.connectUser[user]
+      const targetSocket = req.connectUser[idDev]
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit('match', targetDev)
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit('match', loggedDev)
+      }
+    }
+
     loggedDev.likes.push(targetDev._id)
 
     await loggedDev.save()
